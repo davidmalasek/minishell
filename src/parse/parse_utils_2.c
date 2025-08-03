760515a8894759756b52167418383526458ed660
David Malasek <dmalasek@c2r5s4.42prague.com>
2025-08-03 17:12:33 +0200

    Fixed leak caused by ft_strdup()

--- src/parse/parse_utils_2.c
+++ src/parse/parse_utils_2.c
@@ -6,7 +6,7 @@
 /*   By: dmalasek <dmalasek@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 12:25:07 by dmalasek          #+#    #+#             */
-/*   Updated: 2025/08/02 12:26:16 by dmalasek         ###   ########.fr       */
+/*   Updated: 2025/08/03 17:08:25 by dmalasek         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,11 +38,21 @@ size_t	count_args(t_token *tokens, size_t index)
 void	free_commands(t_command *commands, size_t count)
 {
 	size_t	i;
+	size_t	j;
 
 	i = 0;
 	while (i < count)
 	{
-		free(commands[i].args);
+		if (commands[i].args)
+		{
+			j = 0;
+			while (commands[i].args[j])
+			{
+				free(commands[i].args[j]);
+				j++;
+			}
+			free(commands[i].args);
+		}
 		free(commands[i].infile);
 		free(commands[i].outfile);
 		free(commands[i].heredoc_delim);
